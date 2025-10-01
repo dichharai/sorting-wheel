a94ddd8d72604ba6001ab0530c39a4b6deb6a464
Dichha Rai <dianeraie@gmail.com>
2025-09-30 23:56:50 -0500

    shout out chord with confetti fire

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 "use client";
 
 import Image from "next/image";
+import * as Tone from "tone";
 import shuffleIcon from "../public/images/shuffle.svg";
 import sortIcon from "../public/images/sort.svg";
 import deleteIcon from "../public/images/delete.svg";
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 
 function HomePage() {
   const [textAreaValue, setTextAreaValue] = useState(
@@ -21,6 +22,7 @@ function HomePage() {
   const [ascSort, setAscSort] = useState(false);
   const [showPickedContestantBox, setShowPickedContestantBox] = useState(false);
   const [pickedContestant, setPickedContestant] = useState(null);
+  const [audioContextIsReady, setAudioContextIsReady] = useState(false);
 
   const spinCount = useRef(0);
   const confettiRef = useRef(null);
@@ -84,6 +86,51 @@ function HomePage() {
     });
   }, []);
 
+  const startAudioContext = useCallback(async () => {
+    if (!Tone) {
+      console.log("tone js package is not loaded");
+      return false;
+    }
+    console.log(`context state: ${Tone.getContext().state}`);
+    if (Tone.getContext().state !== "running") {
+      try {
+        await Tone.start();
+        setAudioContextIsReady(true);
+        return true;
+      } catch (error) {
+        console.log(`Failed to start Tonel.js Audio Context. error: ${error}`);
+        return false;
+      }
+    } else {
+      setAudioContextIsReady(true);
+      return true;
+    }
+  }, []);
+
+  const playApplause = useCallback(() => {
+    if (!Tone) {
+      console.log("no tone js library");
+      return;
+    }
+    Tone.getContext()
+      .resume()
+      .then(() => {
+        // 1. Create a synth (PolySynth for a fuller sound)
+        const synth = new Tone.PolySynth(Tone.AMSynth, {
+          oscillator: { type: "triangle" },
+        }).toDestination();
+
+        //2. Play C Major chord for 1 second
+        const now = Tone.now();
+        synth.triggerAttackRelease(["C4", "E4", "G4"], "1n", now);
+
+        // Schedule the synth to be cleaned up after the note finishes
+        Tone.getTransport().scheduleOnce(() => {
+          synth.dispose(); // Removes the synth from the audio graph
+        }, now + 1.5);
+      });
+  }, []);
+
   const handleSpin = async () => {
     if (spinning || options.length === 0) {
       return;
@@ -106,7 +153,7 @@ function HomePage() {
 
     setTransform(`rotate(${finalDegree}deg)`);
 
-    setTimeout(() => {
+    setTimeout(async () => {
       setSpinning(false);
       const contestantName = options[randomIndex];
       setOrderCount((orderCount) => orderCount + 1);
@@ -118,6 +165,14 @@ function HomePage() {
 
       if (confettiRef.current) {
         fireConfetti();
+      }
+
+      const audioStarted = await startAudioContext();
+      if (audioStarted) {
+        console.log(`audio started: ${Tone.getContext().state}`);
+        playApplause();
+      } else {
+        console.log(`audio has not started: ${Tone.getContext().state}`);
       }
 
       setShowPickedContestantBox(true);
